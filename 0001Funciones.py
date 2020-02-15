03f22147efb317e306b5bd725d5283249c5bfccb
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-02-14 19:56:34 -0600

    Funciones basicas y llamada de funciones

---
+++ 0001Funciones.py
@@ -0,0 +1,12 @@
+#PRIMERA FUNCION
+
+def mensaje(): #DECLARANDO FUNCION
+#    CUERPO DE LA FUNCION
+    print("Estoy aprendiendo Python")
+    print("Estoy aprendiendo instrucciones basicas")
+    print("Poco a poco avanzando")
+    
+#LLAMANDO FUNCION
+mensaje()
+print("Ejecutando codigo fuera de funcion")
+mensaje()
---
+++ README.md
@@ -0,0 +1,1 @@
+Curso Pyhton Py
