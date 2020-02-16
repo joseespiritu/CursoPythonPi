3f0537a1a78053de7a77f2e05cda8d4fa91debb4
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-02-15 18:59:32 -0600

    Condicional Else, If, Elif)

---
+++ 0007Condicionales.py
@@ -0,0 +1,29 @@
+#CONDICIONALES IF ELSE ELIF
+print("Verificacion de acceso")
+
+edad_usuario=int(input("Introduce tu edad, por favor: "))
+
+if edad_usuario<18:
+	print("No puedes pasar")
+elif edad_usuario>100:
+	print("Edad incorrecta")
+else:
+	print("Puedes pasar")
+print("El programa 1 ha finalizado")
+
+nota_alumno=int(input("Introduce tu nota, por favor: "))
+
+if nota_alumno<5:
+	print("Insuficiente")
+
+elif nota_alumno<6:
+	print("Suficiente")
+
+elif: nota_alumno<7:
+	print("Bien")
+
+elif nota_alumno<9:
+	print("Notable")
+
+else:
+	print("Sobresaliente")
