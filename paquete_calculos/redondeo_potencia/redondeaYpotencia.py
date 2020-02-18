73a370d94473d9f21e630304d6a16a3c25c2731c
joseespiritu <joseespiritu@aragon.unam.mx>
2020-02-18 16:25:43 -0600

    Uso de paquetes, subpaquetes, modulo init

---
+++ paquete_calculos/redondeo_potencia/redondeaYpotencia.py
@@ -0,0 +1,5 @@
+def potencia(base,exponente):
+    print("El resultado de la multiplicacion es: ",base**exponente)
+    
+def redondear(numero):
+    print("El resultado de la multiplicacion es: ",round(numero))
---
+++ uso_modulos.py
@@ -0,0 +1,7 @@
+#from paquete_calculos.calculos_generales import *
+#USANDO SUBPAQUETES
+from paquete_calculos.subpaquete_basicos.operaciones_basicas import *
+from paquete_calculos.redondeo_potencia.redondeaYpotencia import *
+
+sumar(5,7)
+potencia(5,7)
